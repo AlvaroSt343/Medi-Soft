da2bd2a1cedb2b512beb86403a42cdaedc91d9f4
AlvaroSt343 <red.fear@hotmail.com>
2019-05-29 19:58:40 -0500

    claese coneccion

--- Medi-Soft/Clases/MyConnection.cs
+++ Medi-Soft/Clases/MyConnection.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Medi_Soft.Clases
 {
     class MyConnection
     {
+        public MySqlConnection conexion;
+
+        public void abrirConexion()
+        {
+            String servidor = "localhost";
+            String puerto = "3306";
+            String usuario = "admin";
+            String password = "admin";
+            String database = "Hospital_BD";
+
+            //Esta es la cadena para abrir la conexion
+            String laConexion = String.Format("server={0};port={1};user id={2}; password={3}; database={4}", servidor, puerto, usuario, password, database);
+            conexion = new MySqlConnection(laConexion);
+            conexion.Open();
+        }
+
+        public void cerrarConexion()
+        {
+            conexion.Close();
+        }
+
+        public MySqlConnection GetConexion()
+        {
+            return conexion;
+        }
     }
 }
